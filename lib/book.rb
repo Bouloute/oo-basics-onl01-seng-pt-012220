4501528a07ede40a815c9a67a83dbc2c46ca43cf
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-29 01:31:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,9 +6,15 @@ class Book
   attr_reader :title
   attr_accessor :author
   attr_accessor :page_count
+<<<<<<< HEAD
   attr_accessor :genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+=======
+  attr_writer :genre
+
+  def turn_page
+>>>>>>> 904a61e636d7b2f205076dadc60336754a071612
   end
 end
